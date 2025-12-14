1490258ec4850dff9e7443ce384aee73f0625285
chintaLokesh <123749252+Loki1507@users.noreply.github.com>
2025-12-14 22:41:14 +0530

    test 2

--- POM/test_1.spec.js
+++ POM/test_1.spec.js
@@ -1,1 +1,64 @@
-//
+test('adding item to the cart from the whishlist', async ({ page }) => {
+    //required classes & objects
+    const homepage = new Homepage(page);
+    const electronicsPage = new ElectronicsPage(page);
+    const wishlist        = new Wishlist(page)
+
+    //opening a website or navigation page
+    await page.goto('https://demo.nopcommerce.com/');
+
+    //logininto application with valid credentials
+    await homepage.clickOnLogIN();
+    await homepage.loginWithValidCredentials('testking902@gmail.com', 'testking902');
+    const no_ofItemsInWishlist = await page.locator('.ico-wishlist .wishlist-qty').textContent();
+
+    //checking if the wishlist is empty then we add iteam if not we proceed to wishlist directly
+    if (no_ofItemsInWishlist === '(0)') {
+        //home page - clicking on category
+        await homepage.clickOnElectronics();
+        //capturing all subcategiries in category
+        const subcatogiries = await electronicsPage.captureSubcatgiry('Electronics');
+
+        //choosing subcat type
+        const item = subcatogiries[1];
+        //clicking on sub category
+        await electronicsPage.clickOnSubCatogiry(item);
+        //capture all the products names
+        const allprodutsList = await electronicsPage.captureAllProducts();
+        await page.waitForLoadState();
+
+        //adding single item to wishlist by giving the product name
+        const requiredProduct = 'HTC One Mini Blue'
+        //filtering all the product to find the required product
+        for (let product of allprodutsList) {
+            //console.log(product)
+            if (product === requiredProduct) {
+                //opening the item with the help of product name
+                await electronicsPage.clickOnProductInSubcatpage(product);
+                break
+            }
+        }
+
+        await page.waitForLoadState();
+        //adding item to the cart
+        await electronicsPage.clickOnAddToWishlist();
+        await page.waitForTimeout(3000);
+        await page.waitForLoadState();
+        await wishlist.clickOnWishlistTab();
+
+
+    } else {
+        //open wishlist and add first item to the cart
+        await wishlist.clickOnWishlistTab();
+        await page.waitForLoadState();
+        await page.waitForTimeout(2000);
+    }
+
+    await expect(page.locator('//div[@class="page-title"]//h1')).toContainText('Wishlist')
+    const table = page.locator('//table[@class="cart"]')
+    await table.locator('(//td[@class="add-to-cart"]//input)[1]').check();
+    await page.getByRole('button', { name: /Add to cart/i }).click();
+    await expect( page.locator('//div[@class="page shopping-cart-page"]//h1')).toBeVisible();
+    await page.pause();
+    
+})
